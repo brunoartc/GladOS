4e3bde050f469f23f9575b863ea28ff25ccaf573
ViniGl <vinicius.gl098@gmail.com>
2018-03-09 09:45:38 -0300

    Update And16.vhd

--- Projetos/C-LogicaCombinacional/src/rtl/And16.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/And16.vhd
@@ -7,3 +7,7 @@ entity And16 is
 			b:   in  STD_LOGIC_VECTOR(15 downto 0);
 			q:   out STD_LOGIC_VECTOR(15 downto 0));
 end entity;
+architecture func of And16 is 
+begin
+   F <= a and b;
+end func;
