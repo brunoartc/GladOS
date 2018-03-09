54c4578d1abc72f97ab57827e60f90940ec17c76
ViniGl <vinicius.gl098@gmail.com>
2018-03-09 11:44:22 -0300

    Update Mux8Way16.vhd

--- Projetos/C-LogicaCombinacional/src/rtl/Mux8Way16.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/Mux8Way16.vhd
@@ -14,3 +14,16 @@ entity Mux8Way16 is
 			sel: in  STD_LOGIC_VECTOR(2 downto 0);
 			q:   out STD_LOGIC_VECTOR(15 downto 0));
 end entity;
+
+architecture func of Mux8Way16 is
+ begin
+  q <=a when (sel ='0') else
+      b when (sel = '1') else
+      c when (sel ='2') else
+      d when (sel = '3') else
+      e when (sel ='4') else
+      f when (sel = '5') else
+      g when (sel ='6') else
+      h when (sel = '7') else;
+
+ end func;
