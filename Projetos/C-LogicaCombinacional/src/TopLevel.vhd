344cf616565f2cf941ee49326ebf459e123c3747
wesleygas <wesleygl16@gmail.com>
2018-03-14 19:50:35 -0300

    Implementcao da eq do grupo

--- Projetos/C-LogicaCombinacional/src/TopLevel.vhd
+++ Projetos/C-LogicaCombinacional/src/TopLevel.vhd
@@ -32,12 +32,67 @@ architecture rtl of TopLevel is
 --------------
 -- signals
 --------------
-
+  signal a : std_logic_vector(15 downto 0) := x"0E73"; -- 163
+  signal v : std_logic_vector(15 downto 0) := x"025F"; -- 95
 ---------------
 -- implementacao
 ---------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity And16 is
+	port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			b:   in  STD_LOGIC_VECTOR(15 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
+end entity;
+
+architecture func of ANd16 is 
 begin
+   q <= a and b;
+end func;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Or16 is
+	port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			b:   in  STD_LOGIC_VECTOR(15 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
+end entity;
+
+architecture func of Or16 is 
+begin
+   q <= a or b;
+end func;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Not16 is
+	port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
+end entity;
+	
+architecture func of Not16 is
+  begin
+  q <= not a;
+
+ end func;
+
+
+
  
-  LEDR(0) <= SW(0);
+ begin
+ 
+  juro_que_tentei : Not16 port map(
+	a_signal => a,
+	q => v
+  );
+ 
+  
 
 end rtl;
