05db42db11db748af93f5c0be34c3408912197d1
miannoni <matteoi@al.insper.edu.br>
2018-03-13 21:10:27 -0300

    Matteo 21:09 13/03/2018
    Dmux4way, Dmux8way e correção do Nor8way

--- Projetos/C-LogicaCombinacional/src/rtl/DMux8Way.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/DMux8Way.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity DMux8Way is
-	port ( 
+	port (
 			a:   in  STD_LOGIC;
 			sel: in  STD_LOGIC_VECTOR(2 downto 0);
 			q0:  out STD_LOGIC;
@@ -14,3 +14,16 @@ entity DMux8Way is
 			q6:  out STD_LOGIC;
 			q7:  out STD_LOGIC);
 end entity;
+
+architecture func of DMux8Way is
+
+begin
+	q0 <= a and not sel(2) and not sel(1) and not sel(0);
+	q1 <= a and not sel(2) and not sel(1) and sel(0);
+	q2 <= a and not sel(2) and sel(1) and not sel(0);
+	q3 <= a and not sel(2) and sel(1) and sel(0);
+	q4 <= a and sel(2) and not sel(1) and not sel(0);
+	q5 <= a and sel(2) and not sel(1) and sel(0);
+	q6 <= a and sel(2) and sel(1) and not sel(0);
+	q7 <= a and sel(2) and sel(1) and sel(0);
+end func;
