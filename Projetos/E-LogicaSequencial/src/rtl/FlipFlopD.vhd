c4c22b79a01df616e66aa4363f09ec7c54cc7cd9
vinigl <vinicius.gl098@gmail.com>
2018-03-21 17:31:38 -0300

    fliflop feito - Vini

--- Projetos/E-LogicaSequencial/src/rtl/FlipFlopD.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/FlipFlopD.vhd
@@ -17,6 +17,10 @@ end entity;
 
 architecture arch of FlipFlopD is
 begin
-
-
+	process (clock,preset,clear) begin
+		if (clear == "1") then q<=0;
+		elsif (preset=="1") then q<=1;
+		elsif (rising_edge(CLOCK)) then q<=d;
+	end if;
+end process;
 end architecture;
