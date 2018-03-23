573a7fefaa81cec5e235534e35ff4d4e544305be
vinigl <vinicius.gl098@gmail.com>
2018-03-23 10:53:12 -0300

    Register 8 feito por Vini e register 16 feito por Vini e Bruno

--- Projetos/E-LogicaSequencial/src/rtl/Register8.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/Register8.vhd
@@ -13,3 +13,24 @@ entity Register8 is
 		output: out STD_LOGIC_VECTOR(7 downto 0)
 	);
 end entity;
+
+architecture func of Register8 is 
+component BinaryDigit is
+	port(
+		clock:   in STD_LOGIC; --
+		input:   in STD_LOGIC; --
+		load:    in STD_LOGIC; --
+		output: out STD_LOGIC
+	);
+end component;
+
+	begin 
+	P0: BinaryDigit port map(clock,input(0),load,output(0));
+	P1: BinaryDigit port map(clock,input(1),load,output(1));
+	P2: BinaryDigit port map(clock,input(2),load,output(2));
+	P3: BinaryDigit port map(clock,input(3),load,output(3));
+	P4: BinaryDigit port map(clock,input(4),load,output(4));
+	P5: BinaryDigit port map(clock,input(5),load,output(5));
+	P6: BinaryDigit port map(clock,input(6),load,output(6));
+	P7: BinaryDigit port map(clock,input(7),load,output(7));
+end func;
